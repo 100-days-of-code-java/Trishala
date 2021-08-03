5e3520993e07363d61aa3a455b144dfd30a62b46
Trishala Swain <trishala.190410107138@gmail.com>
2021-08-03 23:49:00 +0530

    Add files via upload

---
+++ Day_5/Solutions/DecimalConverter.java
@@ -0,0 +1,21 @@
+public class DecimalConverter {
+    public static void main(String[] args) {
+
+        areEqualByThreeDecimalPlaces(-2.1756, -2.175);
+        
+    }
+
+
+    public static boolean areEqualByThreeDecimalPlaces(double one, double two) {
+        int a = (int) (one * 1000);
+        int b = (int) (two * 1000);
+        if(a==b) {
+            System.out.println("True");
+            return true;
+        } else {
+            System.out.println("False");
+            return false;
+        }
+        
+    }
+}
